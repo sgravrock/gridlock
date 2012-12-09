445f079a09fbee2ac0c72836b8d874302866fa30
Steve Gravrock <sdg@panix.com>
2012-12-08 16:28:59 -0800

    Initial project skeleton

---
+++ Gridlock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gridlock
+//
+//  Created by Steve Gravrock on 12/8/12.
+//  Copyright (c) 2012 Steve Gravrock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gridlock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gridlock
+//
+//  Created by Steve Gravrock on 12/8/12.
+//  Copyright (c) 2012 Steve Gravrock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
